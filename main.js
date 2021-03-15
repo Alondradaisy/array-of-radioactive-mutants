fe7a96ac52824c7a1c959ae2a14fc7172ff630ba
Alondra Warren <daisy.codeimmersives.com>
2021-03-14 23:41:37 -0700

    work through function mutations

--- main.js
+++ main.js
@@ -2,9 +2,43 @@
  * YOUR CODE HERE *
  ******************/
 
+const arr1 = ['p1','p2']
+function addMeToEnd('Colin') {
+  arr1.push(addMeToEnd); 
+};
 
+function addMeToStart('Colin') {
+  const arr2 = ['check', 'check']
+  arr2.unshift(addMeToStart);
+};
 
+const arr = [num1, num2, num3]
+function changeLast(arr) {
+  arr.splice(changeLast);
+};
 
+const arr3 = [num1, num2, num3]
+function changeAllValuesTo(arr3) {
+  arr3.splice(0,3);
+};
+
+const signs = [arr]
+function oddOrEven(signs) {
+  let odd = 1;
+  let even = 0;
+  for (let i = 0; i < arr.signs; i++) {
+    if (arr[i] & 1 === 1)
+    return odd;
+  } else {
+    return even;
+  }
+};
+
+const consecutiveValues = [value1, value2, value3, value4]
+function changeNextThreeToValue(consecutiveValues) {
+  const startingIndex = 2
+  consecutiveValues.splice(2,3);
+} 
 
 /*********************************
  * OUR CODE BELOW; DO NOT TOUCH! *
